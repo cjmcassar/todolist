3d57ee36c651b45df4fe5b0eb4dad69be3b2def0
Christopher-John Cassar <christopher-john.cassar.16@ucl.ac.uk>
2020-11-06 16:13:00 -0500

    added styling to the list

--- app.js
+++ app.js
@@ -5,10 +5,12 @@ const bodyParser = require("body-parser");
 const app = express();
 
 let items = ["Buy Food", "Cook Food", "Eat Food"];
+let workItems= [];
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 app.get("/", function(req, res)
 {
@@ -24,7 +26,7 @@ app.get("/", function(req, res)
 
   res.render("list",
   {
-    kindofDay: day,
+    listTitle: day,
     newListItems: items
   });
 });
@@ -33,9 +35,38 @@ app.post("/", function(req, res)
 {
   let item = req.body.newItem;
 
+  if (req.body.list ==="Work")
+  {
+    workItems.push(item);
+    res.redirect("/work");
+  }
+  else
+  {
+    items.push(item);
+    res.redirect("/");
+  }
+
+
   items.push(item);
 
   res.redirect("/");
+});
+
+app.get("/work", function(req, res)
+{
+  res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
+
+app.post("/work", function(req, res)
+{
+  let item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
+})
+
+app.get("/about", function(req, res)
+{
+  res.render("about");
 });
 
 app.listen(3000, function()
