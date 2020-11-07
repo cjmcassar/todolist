cdef3b69ca1da8c290602db95c53ba6d1a3f50a2
Christopher-John Cassar <christopher-john.cassar.16@ucl.ac.uk>
2020-11-06 20:08:55 -0500

    -refactored code - created date.js file for functionality outside of the app.js

--- app.js
+++ app.js
@@ -2,10 +2,12 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
+
 const app = express();
 
-let items = ["Buy Food", "Cook Food", "Eat Food"];
-let workItems= [];
+const items = ["Buy Food", "Cook Food", "Eat Food"];
+const workItems= [];
 
 app.set('view engine', 'ejs');
 
@@ -14,15 +16,7 @@ app.use(express.static("public"));
 
 app.get("/", function(req, res)
 {
-  let today = new Date();
-
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long"
-  };
-
-  let day = today.toLocaleDateString("en-US", options);
+  const day = date.getDate();
 
   res.render("list",
   {
@@ -33,7 +27,7 @@ app.get("/", function(req, res)
 
 app.post("/", function(req, res)
 {
-  let item = req.body.newItem;
+  const item = req.body.newItem;
 
   if (req.body.list ==="Work")
   {
